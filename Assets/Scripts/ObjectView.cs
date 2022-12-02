16116cd3014a72528c84324d2bd3c845436af740
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2022-12-02 18:44:44 +0000

    change light, change gama, change rotations

---
+++ Assets/Scripts/ObjectView.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectView : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 20.0f;
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        Vector3 rotatObjx = Camera.main.transform.TransformDirection(new Vector3(0f, 1f, 0f)) ;
+        Vector3 rotatObjy = Camera.main.transform.TransformDirection(new Vector3( 1f, 0f, 0f));
+
+        transform.RotateAround(transform.position, rotatObjy, mouseY);
+        transform.RotateAround(transform.position, rotatObjx, -mouseX);
+    }
+}
---
+++ Assets/Scripts/Rune.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rune : MonoBehaviour
+{
+    public float minDist = 5;
+    public float maxDist = 10;
+    public float displayDist = 15;
+    public float tolerance = 10.0f;
+    public float maxEmission = 0.7f;
+
+    public Transform player;
+
+    Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var mr = GetComponent<MeshRenderer>();
+
+        material = new Material(mr.material);
+        mr.material = material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+/*        if ((distance >= minDist) && (distance <= maxDist))
+        {*/
+            float meanDist = (maxDist + minDist) * 0.5f;
+            float winDist = (maxDist - minDist) * 0.5f;
+            float factor = 1 - Mathf.Abs(Mathf.Clamp((distance - meanDist) / winDist, -1, 1));
+
+            float tol = Mathf.Cos(tolerance * Mathf.Deg2Rad);
+            float dp = Vector3.Dot(transform.forward, player.forward);
+            dp = Mathf.Clamp01((dp - tol) / (1.0f - tol));
+
+            factor = Mathf.Min(factor * dp, maxEmission);
+            //factor = factor * dp;
+
+            Debug.Log(factor);
+
+            material.EnableKeyword("_EMISSION");
+            material.SetColor("_EmissionColor", Color.green * factor);
+/*        }
+        else
+        {
+            material.DisableKeyword("_EMISSION");
+            material.SetColor("_EmissionColor", Color.black);
+        }*/
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, minDist);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, maxDist);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, displayDist);
+    }
+}
