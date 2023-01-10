6cd60a3a0ea44756571761420a3417f1b0868128
Micael Teixeira <mikatx2@hotmail.com>
2023-01-10 12:19:17 +0000

    Fix scripts names, find and change script values to previous ones

--- Assets/Scripts/ItemUsed.cs
+++ Assets/Scripts/ItemUsed.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Item_Used : MonoBehaviour
+public class ItemUsed : MonoBehaviour
 {
     public Interactive Item;
     [SerializeField] private TextMeshProUGUI TotalDone;
